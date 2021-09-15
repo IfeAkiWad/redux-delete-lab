c6a6fbdf7c915cad6d75d627adcaf2617eb5d17d
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-09-15 13:31:15 -0500

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // render a list of bands provided by props
 const Bands = (props) => {
-    const getBands = props.bands.map((band, index) => <li key={index}>{band}</li>)
+    const getBands = props.bands.map((band, index) => <li key={index}>{band.name}</li>)
     return (
         <div>
             {getBands}
